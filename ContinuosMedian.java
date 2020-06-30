9ffd24d0176099563a53c4a5ecfb0b6ac69159fe
yapdianhao <yapdhao@gmail.com>
2020-07-01 01:26:40 +0800

    rename continuos median

--- ContinuosMedian.java
+++ ContinuosMedian.java
@@ -1,100 +1,55 @@
-import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Collections;
 
-class ContinuosMedian {
+class ContMed {
 
     static Kattio io = new Kattio(System.in);
 
     public static void main(String[] args) {
         int cases = io.getInt();
-        for (int i = 0; i < cases; i++) {
-            solve(); 
+        while (cases-- > 0) {
+            solve();
         }
         io.close();
     }
 
     static void solve() {
-        int size = io.getInt();
-        int[] arr = new int[size];
-        for (int i = 0; i < size; i++) {
-            arr[i] = io.getInt();
-        }
-        //Arrays.sort(arr);
-        io.println(genMedian(arr));
-    }
-
-    static long genMedian(int[] arr) {
-        long res = 0;
-        for (int i = 0; i < arr.length; i++) {
-            int[] temp = Arrays.copyOf(arr, i + 1);
-            Arrays.sort(temp);
-            if (i % 2 != 0) {
-                res += (temp[i / 2] + temp[i / 2 + 1]) / 2;
-            } else {
-                res += temp[i / 2];
+        int nums = io.getInt();
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(nums, Collections.reverseOrder());
+        long total = 0;
+        while(nums-- > 0) {
+            int n = io.getInt();
+            if (!minHeap.isEmpty() && n > minHeap.peek()) minHeap.add(n);
+            else maxHeap.add(n);
+            if (maxHeap.size() < minHeap.size()) maxHeap.add(minHeap.poll());
+            else if (maxHeap.size() - minHeap.size() > 1) minHeap.add(maxHeap.poll());
+            if (maxHeap.size() == minHeap.size()) total += (minHeap.peek() + maxHeap.peek()) / 2;
+            else {
+                if (maxHeap.size() > minHeap.size()) total += maxHeap.peek();
+                else total += minHeap.peek();
             }
-            //Arrays.sort(temp);
-            //io.println(Arrays.toString(temp));
-            /*if (i == 0) {
-                res += arr[0];
-                continue;
-            } else if (i % 2 == 1) {
-                PriorityQueue<Integer> minHeap = new PriorityQueue<>(i / 2 + 1);
-                PriorityQueue<Integer> maxHeap = new PriorityQueue<>(i / 2 + 1, Collections.reverseOrder());
-                for (int j = 0; j <= i; j++) {
-                    if (minHeap.isEmpty()) minHeap.add(arr[j]);
-                    else if (minHeap.size() - maxHeap.size() >= 1 && arr[j] > minHeap.peek()) {
-                        if (arr[j] == 7) System.out.println(minHeap.peek() + "fuck");
-                        maxHeap.add(minHeap.poll());
-                        minHeap.add(arr[j]);
-                    } else if (minHeap.size() - maxHeap.size() >= 1 && arr[j] < minHeap.peek()) {
-                        if (arr[j] == 7) System.out.println(minHeap.peek() + "fuck");
-                        maxHeap.add(minHeap.poll());
-                        minHeap.add(arr[j]);
-                    } else {
-                        if (minHeap.peek() < maxHeap.peek()) {
-                            int temp = minHeap.poll();
-                            minHeap.add(maxHeap.poll());
-                            minHeap.add(arr[j]);
-                            maxHeap.add(temp);
-                        } else minHeap.add(arr[j]);
-                    }
-                }
-                io.println("=======" + i + "=======");
-                io.println("min: " + minHeap);
-                io.println("max: " + maxHeap);
-                res += (minHeap.peek() + maxHeap.peek()) / 2;
-                //io.println("res: " + res);
-            } else if (i % 2 == 0) {
-                PriorityQueue<Integer> minHeap = new PriorityQueue<>(i / 2 + 1);
-                PriorityQueue<Integer> maxHeap = new PriorityQueue<>(i / 2 + 1, Collections.reverseOrder());
-                for (int j = 0; j <= i; j++) {
-                    if (minHeap.isEmpty()) minHeap.add(arr[j]);
-                    else if (minHeap.size() - maxHeap.size() >= 1 && arr[j] > minHeap.peek()) {
-                        if (arr[j] == 7) System.out.println(minHeap.peek() + "fuck");
-                        maxHeap.add(minHeap.poll());
-                        minHeap.add(arr[j]);
-                    } else if (minHeap.size() - maxHeap.size() >= 1 && arr[j] < minHeap.peek()) {
-                        if (arr[j] == 7) System.out.println(minHeap.peek() + "fuck");
-                        maxHeap.add(minHeap.poll());
-                        minHeap.add(arr[j]);
-                    } else {
-                        if (minHeap.peek() < maxHeap.peek()) {
-                            int temp = minHeap.poll();
-                            minHeap.add(maxHeap.poll());
-                            minHeap.add(arr[j]);
-                            maxHeap.add(temp);
-                        } else minHeap.add(arr[j]);
-                    }
-                } 
-                io.println("=======" + i + "=======");
-                io.println("min: " + minHeap);
-                io.println("max: " + maxHeap);
-                res += minHeap.peek();    
-                //io.println("res: " + res);     */     
-            //io.println(res);
+            /*
+            minHeap.add(n);
+            //if (minHeap.size() - maxHeap.size() > 1) maxHeap.add(minHeap.poll());
+            if (!maxHeap.isEmpty() && minHeap.peek() < maxHeap.peek()) {
+                int temp = minHeap.poll();
+                minHeap.add(maxHeap.poll());
+                maxHeap.add(temp);
+            }
+            if (minHeap.size() - maxHeap.size() > 1) {
+                //System.out.println("here");
+                maxHeap.add(minHeap.poll());
+            }
+            if (minHeap.size() == maxHeap.size()) {
+                total += (minHeap.peek() + maxHeap.peek()) / 2;
+            } else {
+                total += minHeap.peek();
+            }
+            //io.println("MIN: " + minHeap);
+            //io.println("MAX: " + maxHeap);
+            //System.out.println(total);*/
         }
-        return res;
+        io.println(total);
     }
 }
